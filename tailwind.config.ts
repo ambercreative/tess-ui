1a670e39f13735932c5818de1dcb86d83907d406
Sebastiano Guerriero <se.guerriero@gmail.com>
2024-05-15 15:42:17 +0200

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,59 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          lighter: "hsl(var(--color-primary-lighter) / <alpha-value>)",
+          light: "hsl(var(--color-primary-light) / <alpha-value>)",
+          DEFAULT: "hsl(var(--color-primary) / <alpha-value>)",
+          dark: "hsl(var(--color-primary-dark) / <alpha-value>)",
+          darker: "hsl(var(--color-primary-darker) / <alpha-value>)",
+        },
+        success: {
+          lighter: "hsl(var(--color-success-lighter) / <alpha-value>)",
+          light: "hsl(var(--color-success-light) / <alpha-value>)",
+          DEFAULT: "hsl(var(--color-success) / <alpha-value>)",
+          dark: "hsl(var(--color-success-dark) / <alpha-value>)",
+          darker: "hsl(var(--color-success-darker) / <alpha-value>)",
+        },
+        error: {
+          lighter: "hsl(var(--color-error-lighter) / <alpha-value>)",
+          light: "hsl(var(--color-error-light) / <alpha-value>)",
+          DEFAULT: "hsl(var(--color-error) / <alpha-value>)",
+          dark: "hsl(var(--color-error-dark) / <alpha-value>)",
+          darker: "hsl(var(--color-error-darker) / <alpha-value>)",
+        },
+        warning: {
+          lighter: "hsl(var(--color-warning-lighter) / <alpha-value>)",
+          light: "hsl(var(--color-warning-light) / <alpha-value>)",
+          DEFAULT: "hsl(var(--color-warning) / <alpha-value>)",
+          dark: "hsl(var(--color-warning-dark) / <alpha-value>)",
+          darker: "hsl(var(--color-warning-darker) / <alpha-value>)",
+        },
+        bg: {
+          lighter: "hsl(var(--color-bg-lighter) / <alpha-value>)",
+          light: "hsl(var(--color-bg-light) / <alpha-value>)",
+          DEFAULT: "hsl(var(--color-bg) / <alpha-value>)",
+          dark: "hsl(var(--color-bg-dark) / <alpha-value>)",
+          darker: "hsl(var(--color-bg-darker) / <alpha-value>)",
+        },
+        contrast: {
+          lower: "hsl(var(--color-contrast-lower) / <alpha-value>)",
+          low: "hsl(var(--color-contrast-low) / <alpha-value>)",
+          medium: "hsl(var(--color-contrast-medium) / <alpha-value>)",
+          high: "hsl(var(--color-contrast-high) / <alpha-value>)",
+          higher: "hsl(var(--color-contrast-higher) / <alpha-value>)",
+        },
+      },
+      fontFamily: {
+        'body': ['ui-sans-serif', 'system-ui', 'Inter', 'sans-serif'],
+        'heading': ['ui-sans-serif', 'system-ui', 'Inter', 'sans-serif'],
+      },
+      borderRadius: {
+        'inherit': 'inherit',
+      },
+    },
   },
   plugins: [],
 };
