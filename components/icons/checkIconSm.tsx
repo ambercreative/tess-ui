fa88eaa1e38459aa507e5bc9990adb8897f8ece8
Sebastiano Guerriero <se.guerriero@gmail.com>
2024-05-29 12:51:20 +0200

    checkbox theme=light wip

--- components/icons/checkIconSm.tsx
+++ components/icons/checkIconSm.tsx
@@ -1,13 +1,9 @@
 import {IconProps} from '../lib/definitions';
 
-function CheckIcon(props: IconProps) {
+function CheckIconSm(props: IconProps) {
   const { fill = 'currentColor', secondaryFill = fill, size = '1em', ...rest } = props;
 
 	return (
-		// <svg height={size} width={size} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" {...rest}>
-		//   <polyline fill="none" points="2.75 9.25 6.75 14.25 15.25 3.75" stroke={fill} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
-    // </svg>
-
     <svg height={size} width={size} viewBox="0 0 12 12" {...rest}>
       <g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" stroke="currentColor">
         <path d="m1.75,6c1.047,1.048,1.803,2.153,2.461,3.579,1.524-3.076,3.659-5.397,6.039-7.158"></path>
@@ -16,4 +12,4 @@ function CheckIcon(props: IconProps) {
 	);
 };
 
-export default CheckIcon;
+export default CheckIconSm;
--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react';
 import * as CheckboxRx from '@radix-ui/react-checkbox';
-import CheckIcon from '../icons/checkIcon';
+import CheckIconSm from '../icons/checkIconSm';
 
 type CheckboxRef = React.ElementRef<typeof CheckboxRx.Root>;
 type CheckboxProps = React.ComponentPropsWithoutRef<typeof CheckboxRx.Root>;
@@ -8,9 +8,9 @@ type CheckboxProps = React.ComponentPropsWithoutRef<typeof CheckboxRx.Root>;
 const Checkbox = forwardRef<CheckboxRef, CheckboxProps>
   (({...props}, ref) => {
   return (
-    <CheckboxRx.Root className="h-[14px] w-[14px] appearance-none flex items-center justify-center rounded-[4px] ring-1 ring-shadow/20 shadow-sm bg-surface-light hover:ring-shadow/30 hover:bg-surface-lighter data-[state=checked]:bg-primary data-[state=checked]:text-white data-[state=checked]:ring-primary focus-visible:outline focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-primary" {...props} ref={ref}>
+    <CheckboxRx.Root className="h-[14px] w-[14px] relative appearance-none flex items-center justify-center rounded-[4px] ring-1 ring-shadow/20 shadow-sm bg-surface-light hover:ring-shadow/30 hover:bg-surface-lighter after:absolute after:inset-0 after:rounded-inherit after:shadow-[inset_0_1px_0_hsla(var(--color-white)/var(--inner-glow-opacity))] data-[state=checked]:bg-primary data-[state=checked]:text-white data-[state=checked]:ring-primary focus-visible:outline focus-visible:outline-[1.5px] focus-visible:outline-offset-[3px] focus-visible:outline-primary active:translate-y-px active:transition-transform data-[state=checked]:active:translate-y-0" {...props} ref={ref}>
       <CheckboxRx.Indicator className="text-current">
-        <CheckIcon size="12px" aria-hidden={true}/>
+        <CheckIconSm size="12px" aria-hidden={true}/>
       </CheckboxRx.Indicator>
     </CheckboxRx.Root>
   )
