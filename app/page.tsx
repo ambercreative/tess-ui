d014d6e683f55f8c4658278b5e28ab9ac6078077
Sebastiano Guerriero <se.guerriero@gmail.com>
2024-05-30 11:36:22 +0200

    checkbox component wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import Checkbox from '../components/ui/checkbox';
 
 export default function Home() {
   return (
-    <main className="bg-surface-dark flex justify-center items-center h-lvh">
+    <main className="bg-surface flex justify-center items-center h-lvh">
       <div>
         <Checkbox></Checkbox>
-        {/* <div className="relative h-10 w-80 bg-surface-darker rounded-lg shadow-inner after:absolute after:inset-0 after:rounded-inherit after:shadow-[0_1px_0_hsla(0_0%_100%/1)] dark:after:shadow-[0_1px_0_hsla(0_0%_100%/0.1)]"></div> */}
       </div>
     </main>
   );
--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -8,7 +8,19 @@ type CheckboxProps = React.ComponentPropsWithoutRef<typeof CheckboxRx.Root>;
 const Checkbox = forwardRef<CheckboxRef, CheckboxProps>
   (({...props}, ref) => {
   return (
-    <CheckboxRx.Root className="h-[14px] w-[14px] relative appearance-none flex items-center justify-center rounded-[4px] ring-1 ring-shadow/20 shadow-sm bg-surface-light hover:ring-shadow/30 hover:bg-surface-lighter after:absolute after:inset-0 after:rounded-inherit after:shadow-[inset_0_1px_0_hsla(var(--color-white)/var(--light-opacity))] data-[state=checked]:bg-primary data-[state=checked]:text-white data-[state=checked]:ring-primary focus-visible:outline focus-visible:outline-[1.5px] focus-visible:outline-offset-[3px] focus-visible:outline-primary active:translate-y-px active:transition-transform data-[state=checked]:active:translate-y-0" {...props} ref={ref}>
+    <CheckboxRx.Root className="h-[16px] w-[16px] relative appearance-none flex items-center justify-center rounded-[5px] ring-1 ring-inset ring-contrast-higher/20 shadow-sm bg-surface-light
+
+    hover:ring-contrast-higher/30 hover:bg-surface-lighter 
+    
+    data-[state=checked]:bg-primary data-[state=checked]:text-white data-[state=checked]:ring-primary
+
+    data-[state=checked]:before:absolute data-[state=checked]:before:pointer-events-none data-[state=checked]:before:inset-0 data-[state=checked]:before:rounded-inherit data-[state=checked]:before:shadow-md data-[state=checked]:before:shadow-primary/40 dark:data-[state=checked]:before:shadow-primary/30
+
+    dark:data-[state=checked]:after:absolute dark:data-[state=checked]:after:inset-0 dark:data-[state=checked]:after:rounded-inherit dark:data-[state=checked]:after:pointer-events-none dark:data-[state=checked]:after:shadow-[inset_0_1px_0_hsla(var(--color-white)/0.2)]
+    
+    focus-visible:outline focus-visible:outline-[1.5px] focus-visible:outline-offset-[2px] focus-visible:outline-primary 
+    
+    active:translate-y-px active:transition-transform data-[state=checked]:active:translate-y-0" {...props} ref={ref}>
       <CheckboxRx.Indicator className="text-current">
         <CheckIconSm size="12px" aria-hidden={true}/>
       </CheckboxRx.Indicator>
